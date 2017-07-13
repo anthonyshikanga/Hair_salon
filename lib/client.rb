f3ac12dc39823074033d8c73db20652c21d3e69e
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-07-13 15:20:00 +0300

    modified client.rb file

---
+++ lib/client.rb
@@ -0,0 +1,17 @@
+class Client
+  attr_reader(:name)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+  end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |client|
+      name = client.fetch("name")
+      clients.push(Client.new({:name => description}))
+    end
+    clients
+  end
+  end
