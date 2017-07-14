f8871748d4cbc6f52469820ad6d3693e59271092
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-07-14 08:26:03 +0300

    modified app.rb

--- app.rb
+++ app.rb
@@ -5,4 +5,18 @@ require("sinatra")
   require("pg")
 
   DB = PG.connect({:dbname =>"hair_salon"})
-  
+
+  get("/") do
+    erb(:index)
+  end
+
+  get("/stylists/new") do
+    erb(:stylist_form)
+  end
+
+  post("/stylists") do
+    name = params.fetch("name")
+    stylist = Stylist.new({:name =>name, :id =>nil})
+    stylist.save()
+    erb(:stylist_success)
+  end
